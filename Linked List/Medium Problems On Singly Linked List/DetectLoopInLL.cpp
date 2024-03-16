c2d2f9155934d7dc3040fb41a56fbbcbd33ac7e5
kamehamehaa0000 <gayush794@gmail.com>
2024-03-16 20:28:25 +0530

    Detect Loop in a LL

---
+++ Linked List/Medium Problems On Singly Linked List/DetectLoopInLL.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+// defination of singly linked list
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+/*Brute force - using hashing*/
+bool hasCycle(ListNode *head)
+{
+    unordered_map<ListNode *, int> mpp;
+    ListNode *temp = head;
+    while (temp != nullptr)
+    {
+        if (mpp.find(temp) != mpp.end())
+        { // node already visited
+            return true;
+        }
+        mpp[temp] = 1; // storing current node in hashmap
+        temp = temp->next;
+    }
+    return false;
+}
+/*Optimal using tortoise and hare method*/
+bool hasCycle(ListNode *head)
+{
+    if (head == nullptr)
+    {
+        return false;
+    }
+    ListNode *slow = head;
+    ListNode *fast = head->next;
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        if (slow == fast)
+        {
+            return true;
+        }
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return false;
+}
