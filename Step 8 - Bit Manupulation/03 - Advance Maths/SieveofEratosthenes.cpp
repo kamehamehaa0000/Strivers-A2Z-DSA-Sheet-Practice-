af5c5009146c01828093bdc52b1f6e45766b980e
kamehamehaa0000 <gayush794@gmail.com>
2024-04-03 23:05:01 +0530

    seive of eratosthenes

--- Step 8 - Bit Manupulation/03 - Advance Maths/SieveofEratosthenes.cpp
+++ Step 8 - Bit Manupulation/03 - Advance Maths/SieveofEratosthenes.cpp
@@ -3,6 +3,7 @@ using namespace std;
 // Given an integer n, return the number of prime numbers
 // that are strictly less than n.
 
+// better solution
 int countPrimes(int n)
 {
     int cnt = 0;
@@ -12,7 +13,7 @@ int countPrimes(int n)
         if (prime[i] == 1)
         {
             cnt++;
-            for (int j = i * 2; i < n; j += i)
+            for (int j = i * 2; j < n; j += i)
             {
                 prime[j] = 0;
             }
@@ -20,3 +21,21 @@ int countPrimes(int n)
     }
     return cnt;
 }
+/* //optimising above approach but will not work for the above problem but can be used to find prime till n
+int countPrimes(int n)
+{
+    int cnt = 0;
+    vector<int> prime(n + 1, 1); // assuming every no. as prime
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (prime[i] == 1)
+        {
+            cnt++;
+            for (int j = i * i; j <= n; j += i)
+            {
+                prime[j] = 0;
+            }
+        }
+    }
+    return cnt;
+}*/
